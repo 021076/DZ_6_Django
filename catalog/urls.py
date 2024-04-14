f45eaaecbd934b99f953ad9aeb08413b3931ca0f
ro_k_ <ro_k_sana@mail.ru>
2024-04-14 18:50:07 +0500

    6 курс. Django. ДЗ_1: Знакомство с Django

---
+++ catalog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from catalog.views import home, contacts
+from catalog.apps import CatalogConfig
+
+# from config.main import views
+
+app_name = CatalogConfig.name
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('contacts/', contacts, name='contacts')
+]
